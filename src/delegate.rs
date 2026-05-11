3d61ab2b54d9919afd24eefba34539feef17a0d3
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2026-05-11 17:01:43 +0800

    initial project

---
+++ src/delegate.rs
@@ -0,0 +1,5 @@
+pub fn execute() {
+    let _package_manager = locate_package_manager();
+}
+
+fn locate_package_manager() {}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,14 @@
-/// Application.
-pub mod app;
+mod delegate;
+use std::{env};
 
-/// Terminal events handler.
-pub mod event;
+const PACKAGE_MANAGER: &[&str] = &["bun", "npm", "yarn", "pnpm"];
 
-/// Widget renderer.
-pub mod ui;
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-/// Terminal user interface.
-pub mod tui;
-
-/// Application updater.
-pub mod update;
-use app::App;
-use color_eyre::Result;
-use event::{Event, EventHandler};
-use ratatui::{backend::CrosstermBackend, Terminal};
-use tui::Tui;
-use update::update;
-
-fn main() -> Result<()> {
-    // Create an application.
-    let mut app = App::new();
-
-    // Initialize the terminal user interface.
-    let backend = CrosstermBackend::new(std::io::stderr());
-    let terminal = Terminal::new(backend)?;
-    let events = EventHandler::new(250);
-    let mut tui = Tui::new(terminal, events);
-    tui.enter()?;
-
-    // Start the main loop.
-    while !app.should_quit {
-        // Render the user interface.
-        tui.draw(&mut app)?;
-        // Handle events.
-        match tui.events.next()? {
-            Event::Tick => {}
-            Event::Key(key_event) => update(&mut app, key_event),
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-        };
+    if args.len() >= 2 && PACKAGE_MANAGER.contains(&args[1].as_str()) {
+        println!("{} package manager is compatible with leuko", args[1]);
     }
 
-    // Exit the user interface.
-    tui.exit()?;
-    Ok(())
+    delegate::execute();
 }
