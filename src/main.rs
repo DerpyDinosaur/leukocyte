211f32311b363caa07c5e7309d075d4ecde87dea
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2026-05-21 01:59:14 +0800

    shim: added locate_package_manager

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
-mod delegate;
-use std::{env};
+use clap::Parser;
 
-const PACKAGE_MANAGER: &[&str] = &["bun", "npm", "yarn", "pnpm"];
+mod cli;
+mod lib;
+mod shim;
+
+static PACKAGE_MANAGER: [&str; 4] = ["bun", "npm", "yarn", "pnpm"];
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // Nom all arguments from environment
+    let args = std::env::args().collect();
+    let whatami = leuko::whatami(&args);
 
-    if args.len() >= 2 && PACKAGE_MANAGER.contains(&args[1].as_str()) {
-        println!("{} package manager is compatible with leuko", args[1]);
+    match whatami {
+        "leuko" => run_as_leuko(&args),
+        _ => run_as_shim(&whatami, &args),
     }
+}
 
-    delegate::execute();
+fn run_as_shim(package_manager: &str, args: &Vec<String>) {
+    let _subcommand = args.get(1).map(String::as_str).unwrap_or("");
+
+    // Detect if the user is installing packages
+
+    // If node packages fetch details from node registry
+    // TODO: If python packages fetch details from ???
+
+    // Fetching details for packages could be something leuko does via commands too
+    // Giving the user options, if they do not install the shim for a specific package manager
+
+    match package_manager {
+        "npm" => shim::execute("npm"),
+        "bun" => shim::execute("bun"),
+        _ => panic!("Unsupported package manager!")
+    }
+}
+
+fn run_as_leuko(args: &Vec<String>) {
+    let _cli = cli::Cli::parse();
+    println!("Leuko: Hi im Leuko!");
+
+    // match cli.command {
+    //     cli::Commands::Install { packages, npm_flags } => {
+    //         let mut all_args = packages.clone();
+    //         all_args.extend(npm_flags);
+    //         commands::install::run(&all_args);
+    //     }
+    //     cli::Commands::Audit { packages } => {
+    //         commands::audit::run(&packages);
+    //     }
+    // }
 }
