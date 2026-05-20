5433c3061fde84a7454ecded12f9b3b43bd57aa2
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2026-05-21 01:57:02 +0800

    added boiler plate for clap

---
+++ src/cli.rs
@@ -0,0 +1,18 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "leuko", about = "Friendly white blood cell for your package manager")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Install {
+        packages: Vec<String>
+    },
+    Audit {
+        packages: Vec<String>,
+    },
+}
