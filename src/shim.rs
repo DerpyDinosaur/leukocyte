6c68f21cbc218497bbf8f90bd1c56b37d3097fdc
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2026-05-21 01:59:48 +0800

    wip: main function

---
+++ src/shim.rs
@@ -0,0 +1,76 @@
+use std::env;
+use thiserror::Error;
+use std::path::PathBuf;
+
+#[derive(Debug, Error)]
+enum LocateError {
+    #[error("Failed to get leuko's path")]
+    ExePathUnavailable,
+    #[error("PATH is not defined in the environment")]
+    PathEnvMissing,
+    #[error("Could not find package manager: {0}")]
+    NotFound(String),
+}
+
+pub fn execute(target: &str) {
+    let package_manager_path = locate_package_manager(&target);
+}
+
+fn locate_package_manager(target: &str) -> Result<PathBuf, LocateError> {
+    // parent() -> return parent folder to ignore later when detected in the path env
+    // canonicalize() -> return absolute path, normalized and symbolic links resolved
+    let leuko_dir = match env::current_exe() {
+        Ok(exe) => exe.parent().and_then(|p| p.canonicalize().ok()),
+        Err(_) => return Err(LocateError::ExePathUnavailable),
+    };
+
+    let path_env = env::var_os("PATH").ok_or(LocateError::PathEnvMissing)?;
+
+    for dir in env::split_paths(&path_env) {
+        // Skip paths that are leuko
+        // *leuko is dereferencing the value to make it the same type as dir
+        let is_leuko_dir = leuko_dir
+            .as_ref()
+            .and_then(|leuko| dir.canonicalize().ok().map(|c| c == *leuko))
+            .unwrap_or(false);
+
+        if is_leuko_dir {
+            continue;
+        }
+
+        // Find true path
+        let candidate = dir.join(target);
+        if candidate.exists() {
+            return Ok(candidate);
+        }
+    }
+
+    Err(LocateError::NotFound(target.to_string()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn locate_package_manager_smoke_test() {
+        let result = locate_package_manager("mkdir");
+        assert!(matches!(result, Ok(path) if path.ends_with("mkdir")));
+    }
+
+    #[test]
+    fn locate_package_manager_not_found() {
+        let result = locate_package_manager("this_does_not_exist");
+        assert!(matches!(result, Err(LocateError::NotFound(t)) if t == "this_does_not_exist"));
+    }
+
+    #[test]
+    fn execute_npm() {
+        execute("npm");
+    }
+
+    #[test]
+    fn execute_bun() {
+        execute("bun");
+    }
+}
---
+++ tests/main.rs
@@ -0,0 +1,12 @@
+#[test]
+#[ignore = "This is an example integration test"]
+fn test_binary_output() {
+    let output = std::process::Command::new("cargo")
+        .args(["run", "--", "install"])
+        .output()
+        .expect("failed to execute");
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    println!("{}", stdout);
+    assert!(stdout.contains("leuko"));
+}
